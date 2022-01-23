24c147648798acb5482351cf2bbfaca9e372a227
picu63 <picu63@gmail.com>
2022-01-23 13:55:17 +0100

    Update client app with docker support

--- FinalProject/TodoUI/Program.cs
+++ FinalProject/TodoUI/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
 using TodoUI;
 using TodoUI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Logging.AddSerilog(new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341/")
+    .CreateLogger());
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddHttpClient("TodoApi",client =>
-    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("TodoApi:BaseUrl")));
+builder.Services.AddHttpClient("TodoApi",
+    client => { 
+        client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("TodoApi:BaseUrl"));
+    });
 builder.Services.AddScoped<TodoService>();
 await builder.Build().RunAsync();
--- FinalProject/TodoUI/Services/TodoService.cs
+++ FinalProject/TodoUI/Services/TodoService.cs
@@ -5,21 +5,35 @@ namespace TodoUI.Services;
 
 public class TodoService
 {
+    private readonly ILogger<TodoService> logger;
     private readonly HttpClient httpClient;
 
-    public TodoService(IHttpClientFactory httpClientFactory)
+    public TodoService(IHttpClientFactory httpClientFactory, ILogger<TodoService> logger)
     {
+        this.logger = logger;
         this.httpClient = httpClientFactory.CreateClient("TodoApi");
     }
 
     public async Task<Todo[]> GetAllTodos()
     {
-        return await httpClient.GetFromJsonAsync<Todo[]>(string.Empty);
+        var requestUri = "api/todos";
+        logger.LogInformation($"Getting list of todos from {new Uri(httpClient.BaseAddress, requestUri)}");
+        return await httpClient.GetFromJsonAsync<Todo[]>(requestUri);
     }
 }
 
-public record Todo(Guid Id, string Task, bool Completed)
+public class Todo
 {
+
     [JsonPropertyName("_id")]
-    public Guid Id { get; } = Id;
+    public Guid Id { get; }
+    public string Task { get; init; }
+    public bool Completed { get; init; }
+
+    public void Deconstruct(out Guid Id, out string Task, out bool Completed)
+    {
+        Id = this.Id;
+        Task = this.Task;
+        Completed = this.Completed;
+    }
 }
